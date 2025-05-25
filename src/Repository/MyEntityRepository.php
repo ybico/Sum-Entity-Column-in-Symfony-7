ae63f2cc33778681507c7d0071578924dccbb5d3
ybico <ybico@yahoo.com>
2025-05-25 03:32:51 -0500

    Create MyEntityRepository.php

---
+++ src/Repository/MyEntityRepository.php
@@ -0,0 +1,29 @@
+<php?
+src/Repository/MyEntityRepository.php
+
+namespace App\Repository;
+
+use App\Entity\MyEntity;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+class MyEntityRepository extends ServiceEntityRepository
+{
+
+  public function __construct(ManagerRegistry $registry)
+  {
+      parent::__construct($registry, MyEntity::class);
+  }
+  
+  // function returns the sum of the column order_total as a float type
+  public function SumOrderTotal(): float
+  {
+    $result = $this->createQueryBuilder('myalias')
+        ->select('SUM(myalias.order_total) AS eu1')
+        ->getQuery()
+        ->getSingleScalarResult();
+    // dd($result);  // check outcome
+    return $result;
+  }
+  ...
+}
